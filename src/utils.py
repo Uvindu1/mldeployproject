c3b19ab195d893e762732c750e1ab7e88cfddc23
Uvindu1 <uvinducs153@gmail.com>
2024-03-04 22:12:59 +0530

    Create data trnasformationfile

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,27 @@
 '''
-Database and Cloude base code write here
+Database and Cloude base code write here,
+this is use to usualy using method create here
+    Ex: save folder(this is comman all components)
 '''
+import os
+import sys
+
+import numpy as np 
+import pandas as pd
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
